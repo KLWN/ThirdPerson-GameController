8b1d8f7cded997301d395352293cfe62b6808fc5
Alex <hello@alex-kielwein.com>
2019-05-31 16:26:14 +0200

    Added 2nd Input-Controller with Root-Motion movement

---
+++ Assets/Scripts/InputControllerBT.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+#pragma warning disable
+
+public class InputControllerBT : MonoBehaviour
+{
+    private static Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+
+    private bool isRunning = false;
+    public float vert;
+    public float hori;
+    public bool run;
+    
+
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    
+    private void FixedUpdate()
+    {
+
+        vert = Input.GetAxis("Vertical");
+        hori = Input.GetAxis("Horizontal");
+        run = Input.GetKeyDown(KeyCode.R);
+        
+        if (run)
+        {
+            isRunning = !isRunning;
+        }
+
+        
+        if (!isRunning)
+        {
+            vert *= 0.5f;
+        }
+        
+        
+        m_Animator.SetFloat("Forward", vert, 0.1f, Time.deltaTime);
+        m_Animator.SetFloat("Turn", hori * 3, 0.1f, Time.deltaTime);
+
+        if (Time.deltaTime > 0)
+        {
+            Vector3 velocity = m_Animator.deltaPosition / Time.deltaTime;
+            m_Rigidbody.velocity = velocity;
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Animator.SetTrigger("isJumping");
+        }
+        
+
+    }
+
+    
+    
+    
+}
