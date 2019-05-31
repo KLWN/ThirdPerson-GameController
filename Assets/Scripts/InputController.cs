5b4a889f27d0f8da076885326f511c16d7b09c35
Alex <hello@alex-kielwein.com>
2019-05-31 13:52:11 +0200

    Added Animations

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#pragma warning disable
+
+public class InputController : MonoBehaviour
+{
+
+    
+    
+    
+    
+}
